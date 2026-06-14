20129d53ce7a7b22a615594ebce2d9fb4e673858
RyanAndrewStoryBox <ryan.andrew19@gmail.com>
2026-06-14 21:13:04 +0100

    Flag_updates_v3

--- app.js
+++ app.js
@@ -51,7 +51,7 @@
             var name = m.trim();
             var iso = COUNTRY_ISO[name];
             if (!iso) return '<span class="flag-wrap" title="' + esc(name) + '">' + esc(name) + '</span>';
-            if (iso === 'global') return '<span class="flag-wrap"><span class="flag-tip">' + esc(name) + '</span><img class="flag-img" src="https://img.icons8.com/emoji/32/globe-showing-americas.png" alt="Global"></span>';
+            if (iso === 'global') return '<span class="flag-wrap"><span class="flag-tip">' + esc(name) + '</span><svg class="flag-img" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"><circle cx="12" cy="12" r="10"/><path d="M2 12h20M12 2a15.3 15.3 0 0 1 4 10 15.3 15.3 0 0 1-4 10 15.3 15.3 0 0 1-4-10A15.3 15.3 0 0 1 12 2z"/></svg></span>';
             return '<span class="flag-wrap"><span class="flag-tip">' + esc(name) + '</span><img class="flag-img" src="https://flagcdn.com/w40/' + iso + '.png" alt="' + esc(name) + '"></span>';
         }).join('');
     }
